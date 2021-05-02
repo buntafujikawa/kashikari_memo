5f8a38e740ec84b4b7ecdd62d27727ac3341108c
buntafujikawa <bunta.fujikawa@gmail.com>
2021-05-02 10:44:56 +0900

    add lint

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
-import 'package:flutter/material.dart';
-import 'package:firebase_core/firebase_core.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -27,26 +27,29 @@ class _MyList extends State<List> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("リストと画面"),
+        title: const Text('リストと画面'),
       ),
       body: Padding(
           padding: const EdgeInsets.all(8.0),
           child: StreamBuilder<QuerySnapshot>(
-              stream: FirebaseFirestore.instance.collection('kashikari-memo').snapshots(),
+              stream: FirebaseFirestore.instance
+                  .collection('kashikari-memo')
+                  .snapshots(),
               // streamに変化があった時に呼び出される
-              builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
                 if (!snapshot.hasData) return const Text('Loading...');
                 return ListView.builder(
                   itemCount: snapshot.data!.docs.length,
-                    padding: const EdgeInsets.only(top: 10.0),
-                    itemBuilder: (context, index) => _buildListItem(context, snapshot.data!.docs[index]),
+                  padding: const EdgeInsets.only(top: 10.0),
+                  itemBuilder: (context, index) =>
+                      _buildListItem(context, snapshot.data!.docs[index]),
                 );
-              })
-      ),
+              })),
       floatingActionButton: FloatingActionButton(
         child: const Icon(Icons.add),
         onPressed: () {
-          print("新規作成ボタンを押しました");
+          print('新規作成ボタンを押しました');
         },
       ),
     );
@@ -54,23 +57,22 @@ class _MyList extends State<List> {
 
   Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
     return Card(
-      child: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: <Widget>[
-          ListTile(
-            leading: const Icon(Icons.android),
-            title: Text("【" + (document['borrowOrLend'] == "lend" ? "貸" : "借" ) + "】" + document['stuff']),
-            subtitle: Text('期限: ' + document['date'].toDate().toString().substring(0, 10) + "\n相手: " + document['user']),
-          ),
-          ButtonBar(children: <Widget>[
-            FlatButton(
-                child: const Text("編集"),
-                onPressed: () {
-                  print("編集ボタンを押しました");
-                })
-          ])
-        ]
-      )
-    );
+        child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+      ListTile(
+        leading: const Icon(Icons.android),
+        title: Text(
+            "【${document['borrowOrLend'] == "lend" ? "貸" : "借"}】 ${document['stuff']}"),
+        subtitle: Text(
+            "期限: ${document['date'].toDate().toString().substring(0, 10)}" +
+                "\n相手: ${document['user']}"),
+      ),
+      ButtonBar(children: <Widget>[
+        FlatButton(
+            child: const Text('編集'),
+            onPressed: () {
+              print('編集ボタンを押しました');
+            })
+      ])
+    ]));
   }
 }
